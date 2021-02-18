1d1a6baeadf9bd049c388a4e1fc2eca1b8afc484
leandro falcao <leandrofalcaoti@gmail.com>
2021-02-18 13:25:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function converter() {
-  let polegada = document.querySelector('#pol')
-  .value;
+  
+  // valor da polegada digitada
+  let polegada = document.querySelector('#pol').value;
+  
+  // pegar o valor de cm e depois escrever o tamanho de polegada vezes 2,54 que dar valor em centimetros. 
   let cm = document.querySelector('#cm');
-  cm.innerHTML = `${polegada * 2.54} cm`;  
+  
+    cm.innerHTML = `${polegada * 2.54} cm`;  
 }
